69fe6100d5d3db4b996605132ea2327d63424599
eliankeil <65242046+eliankeil@users.noreply.github.com>
2025-12-21 22:42:34 -0300

    Create gui_skin.h

---
+++ include/gui_skin.h
@@ -0,0 +1,23 @@
+#ifndef _GUI_SKIN_H_
+#define _GUI_SKIN_H_
+#include <stdint.h>
+
+typedef struct {
+    uint64_t background;
+    uint64_t headerText;
+    uint64_t listText;
+    uint64_t selectedText;
+    uint64_t optionText;
+    uint64_t warnText;
+    uint64_t errorText;
+    uint64_t coverFrame;
+    uint64_t iconFrame;
+} ThemeColors;
+
+extern ThemeColors currentTheme;
+
+void setDefaultSkin(void);
+int  loadSkin(const char *path);
+int  saveSkin(const char *path);
+
+#endif
