d35d3ff8b5cb30e5172d284359d4bb555f4b8847
eliankeil <65242046+eliankeil@users.noreply.github.com>
2025-12-22 11:25:01 -0300

    Create skin_layout.h

---
+++ include/skin_layout.h
@@ -0,0 +1,9 @@
+#ifndef _SKIN_LAYOUT_H_
+#define _SKIN_LAYOUT_H_
+
+// Constantes de layout compartidas entre gui.c y gui_skin.c
+extern const int keepoutArea;
+extern const int headerHeight;
+extern const int footerHeight;
+
+#endif
