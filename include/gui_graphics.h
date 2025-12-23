5447a1ac70a3407d4d4c67315bc7c504b79778d3
eliankeil <65242046+eliankeil@users.noreply.github.com>
2025-12-23 14:28:31 -0300

    Update gui_graphics.h

--- include/gui_graphics.h
+++ include/gui_graphics.h
@@ -39,10 +39,8 @@ typedef enum {
   ICON_SELECT,
   ICON_START,
   ICON_ENABLED,
-  ICON_UP,
-  ICON_DOWN,
-  ICON_LEFT,
-  ICON_RIGHT
+  ICON_UPDOWN,
+  ICON_LEFTRIGHT
 } IconType;
 
 // Initializes and uploads graphics resources to GS VRAM
